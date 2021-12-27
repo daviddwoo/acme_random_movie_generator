36d6092f5c28558b089f33a358068af132fec420
David <dwoo1016@gmail.com>
2021-12-27 14:17:46 -0500

    updates

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -2,24 +2,78 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { deleteMovie, incrementRating, decrementRating } from '../store'
 
-const Movies = ({ movies, deleteMovie, incrementRating, decrementRating }) => {
-  return (
-    <div>
-      {
-        movies.map((movie) => {
-          return (
-            <div key={movie.id} className='movieDiv'>
-              <button onClick={() => deleteMovie(movie.id)}>DELETE</button>
-              {movie.name} ({movie.rating})
-              <button onClick={() => decrementRating(movie)}> - </button>
-              <button onClick={() => incrementRating(movie)}> + </button>
-            </div>
-          )
-        })
+// const Movies = ({ movies, deleteMovie, incrementRating, decrementRating }) => {
+//   return (
+//     <div>
+//       {
+//         movies.map((movie) => {
+//           return (
+//             <div key={movie.id} className='movieDiv'>
+//               <button onClick={() => deleteMovie(movie.id)}>DELETE</button>
+//               {movie.name} ({movie.rating})
+//               <button onClick={() => decrementRating(movie)}> - </button>
+//               <button onClick={() => incrementRating(movie)}> + </button>
+//             </div>
+//           )
+//         })
+//       }
+//     </div>
+//   )
+// };
+
+class Movies extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      error: ''
+    }
+  }
+
+  increment = async(movie) => {
+    try {
+      await this.props.incrementRating(movie);
+      if (this.state.error) {
+        this.setState({error: ''})
       }
-    </div>
-  )
-};
+    }
+    catch(ex) {
+      console.log(ex);
+      this.setState({error: ex.response.data})
+    }
+  }
+
+  decrement = async(movie) => {
+    try {
+      await this.props.decrementRating(movie);
+      if (this.state.error) {
+        this.setState({error: ''})
+      }
+    }
+    catch(ex) {
+      console.log(ex);
+      this.setState({error: ex.response.data})
+    }
+  }
+  render() {
+    return (
+      <div>
+        {
+          this.props.movies.map((movie) => {
+            return (
+              <div key={movie.id} className='movieDiv'>
+                <button onClick={() => deleteMovie(movie.id)}>DELETE</button>
+                {movie.name} ({movie.rating})
+                <button onClick={() => this.decrement(movie)}> - </button>
+                <button onClick={() => this.increment(movie)}> + </button>
+                { !!this.state.error && <div> { 'error cant do that!' }</div>}
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
+}
 
 const mapStateToProps = ({ movies }) => {
   return {
